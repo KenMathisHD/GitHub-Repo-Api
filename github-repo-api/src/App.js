22dbab34e5d009e68c55cb1538789af46a1c38ca
Kenneth Simpson <44129752+KenMathisHD@users.noreply.github.com>
2022-03-07 10:22:09 -0600

    seperation of class and functional App component versions complete and successful, slight cleanup done

--- github-repo-api/src/App.js
+++ github-repo-api/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Markdown from "./components/markdown/markdown";
 import Sidebar from "./components/sidebar/sidebar";
@@ -61,64 +61,3 @@ const App = () => {
 };
 
 export default App;
-
-// class App extends Component {
-//   state = {
-//     repos: [],
-//     reposNames: [],
-//     markdown: "",
-//     currentPage: 1,
-//     repoCount: 0,
-//     pageSize: 30,
-//     user: window.location.href.split("/github/")[1],
-//   };
-
-// async componentDidMount() {
-//   const { user, markdown, repoCount, currentPage, pageSize } = this.state;
-//   const totalCount = await getRepoCount(user, repoCount);
-//   const data = await getRepos(user, currentPage);
-//   this.setState({ repos: data, repoCount: totalCount });
-
-//   if (markdown === "") {
-//     const markdown = await getMarkdown(user, getRepo(data, data[0].name));
-//     this.setState({ markdown: marked(markdown) });
-//   }
-// }
-
-//   async handleClick(repo) {
-//     const { user, repos } = this.state;
-//     const markdown = await getMarkdown(user, getRepo(repos, repo));
-//     this.setState({ markdown: marked(markdown) });
-//   }
-
-//   async handleScroll() {
-//     const { currentPage, repos, repoCount, user, pageSize } = this.state;
-//     const nextPage = currentPage + 1;
-//     if (nextPage < repoCount / pageSize + 1) {
-//       const nextRepos = await getRepos(user, nextPage, pageSize);
-//       const updatedRepos = [...repos, ...nextRepos];
-//       this.setState({ repos: updatedRepos, currentPage: nextPage });
-//     }
-//   }
-
-//   render() {
-//     const { repos, markdown } = this.state;
-//     const repoNames = getRepoNames(repos);
-
-//     return (
-//       <div className="app-header">
-//         <Sidebar
-//           repos={repoNames}
-//           handleClick={this.handleClick.bind(this)}
-//           handleScroll={this.handleScroll.bind(this)}
-//         ></Sidebar>
-//         <div className="container">
-//           <h1>GitHub Repository API Frontend</h1>
-//           <Markdown md={markdown}></Markdown>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
