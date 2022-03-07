b445db2c96ff0662c2b682b68974c9d1470111bc
Kenneth Simpson <44129752+KenMathisHD@users.noreply.github.com>
2022-03-07 10:12:34 -0600

    successful conversion from class component to functional component

--- github-repo-api/src/App.js
+++ github-repo-api/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 
 import Markdown from "./components/markdown/markdown";
 import Sidebar from "./components/sidebar/sidebar";
@@ -8,63 +8,117 @@ import { getRepoNames, getRepo } from "./utils/functions";
 import { marked } from "marked";
 import "./App.scss";
 
-class App extends Component {
-  state = {
-    repos: [],
-    reposNames: [],
-    markdown: "",
-    currentPage: 1,
-    repoCount: 0,
-    pageSize: 30,
-    user: window.location.href.split("/github/")[1],
-  };
+const App = () => {
+  const [repos, setRepos] = useState([]);
+  const [markdown, setMarkdown] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [repoCount, setRepoCount] = useState(0);
+  const [pageSize, setPageSize] = useState(30);
+  const [user, setUser] = useState(window.location.href.split("/github/")[1]);
 
-  async componentDidMount() {
-    const { user, markdown, repoCount, currentPage, pageSize } = this.state;
+  useEffect(async () => {
     const totalCount = await getRepoCount(user, repoCount);
     const data = await getRepos(user, currentPage);
-    this.setState({ repos: data, repoCount: totalCount });
+    setRepos(data);
+    setRepoCount(totalCount);
 
     if (markdown === "") {
       const markdown = await getMarkdown(user, getRepo(data, data[0].name));
-      this.setState({ markdown: marked(markdown) });
+      setMarkdown(marked(markdown));
     }
-  }
+  }, []);
 
-  async handleClick(repo) {
-    const { user, repos } = this.state;
+  async function handleClick(repo) {
     const markdown = await getMarkdown(user, getRepo(repos, repo));
-    this.setState({ markdown: marked(markdown) });
+    setMarkdown(marked(markdown));
   }
 
-  async handleScroll() {
-    const { currentPage, repos, repoCount, user, pageSize } = this.state;
+  async function handleScroll() {
     const nextPage = currentPage + 1;
     if (nextPage < repoCount / pageSize + 1) {
       const nextRepos = await getRepos(user, nextPage, pageSize);
       const updatedRepos = [...repos, ...nextRepos];
-      this.setState({ repos: updatedRepos, currentPage: nextPage });
+      setRepos(updatedRepos);
+      setCurrentPage(nextPage);
     }
   }
 
-  render() {
-    const { repos, markdown } = this.state;
-    const repoNames = getRepoNames(repos);
+  const repoNames = getRepoNames(repos);
 
-    return (
-      <div className="app-header">
-        <Sidebar
-          repos={repoNames}
-          handleClick={this.handleClick.bind(this)}
-          handleScroll={this.handleScroll.bind(this)}
-        ></Sidebar>
-        <div className="container">
-          <h1>GitHub Repository API Frontend</h1>
-          <Markdown md={markdown}></Markdown>
-        </div>
+  return (
+    <div className="app-header">
+      <Sidebar
+        repos={repoNames}
+        handleClick={handleClick}
+        handleScroll={handleScroll}
+      ></Sidebar>
+      <div className="container">
+        <h1>GitHub Repository API Frontend</h1>
+        <Markdown md={markdown}></Markdown>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     repos: [],
+//     reposNames: [],
+//     markdown: "",
+//     currentPage: 1,
+//     repoCount: 0,
+//     pageSize: 30,
+//     user: window.location.href.split("/github/")[1],
+//   };
+
+// async componentDidMount() {
+//   const { user, markdown, repoCount, currentPage, pageSize } = this.state;
+//   const totalCount = await getRepoCount(user, repoCount);
+//   const data = await getRepos(user, currentPage);
+//   this.setState({ repos: data, repoCount: totalCount });
+
+//   if (markdown === "") {
+//     const markdown = await getMarkdown(user, getRepo(data, data[0].name));
+//     this.setState({ markdown: marked(markdown) });
+//   }
+// }
+
+//   async handleClick(repo) {
+//     const { user, repos } = this.state;
+//     const markdown = await getMarkdown(user, getRepo(repos, repo));
+//     this.setState({ markdown: marked(markdown) });
+//   }
+
+//   async handleScroll() {
+//     const { currentPage, repos, repoCount, user, pageSize } = this.state;
+//     const nextPage = currentPage + 1;
+//     if (nextPage < repoCount / pageSize + 1) {
+//       const nextRepos = await getRepos(user, nextPage, pageSize);
+//       const updatedRepos = [...repos, ...nextRepos];
+//       this.setState({ repos: updatedRepos, currentPage: nextPage });
+//     }
+//   }
+
+//   render() {
+//     const { repos, markdown } = this.state;
+//     const repoNames = getRepoNames(repos);
+
+//     return (
+//       <div className="app-header">
+//         <Sidebar
+//           repos={repoNames}
+//           handleClick={this.handleClick.bind(this)}
+//           handleScroll={this.handleScroll.bind(this)}
+//         ></Sidebar>
+//         <div className="container">
+//           <h1>GitHub Repository API Frontend</h1>
+//           <Markdown md={markdown}></Markdown>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
